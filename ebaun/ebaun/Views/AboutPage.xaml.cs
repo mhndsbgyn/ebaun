e934e7e3d63d8b9f48d5b5b375126e40d62992fe
mhuseyincekmez@gmail.com <mhuseyincekmez@gmail.com>
2020-04-20 20:51:07 +0300

    basak kendisi deniyor

--- ebaun/ebaun/Views/AboutPage.xaml.cs
+++ ebaun/ebaun/Views/AboutPage.xaml.cs
@@ -13,7 +13,8 @@ namespace ebaun.Views
             InitializeComponent();
             // Proje içine atılmış repositoryde değişiklik yapıp pushluyorumuuum has ya ol
 
-            //buda benden sanaa
+            //buda benden
+            //basak kendisi deniyor
         }
     }
 }
