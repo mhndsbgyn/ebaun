25d5294525fdc973a4cf14170ea6e26ab33f9e44
Gizem Candemir <gizemfener2727@gmail.com>
2020-04-20 20:53:26 +0300

    healalbebize

--- ebaun/ebaun/Views/AboutPage.xaml.cs
+++ ebaun/ebaun/Views/AboutPage.xaml.cs
@@ -11,9 +11,7 @@ namespace ebaun.Views
         public AboutPage()
         {
             InitializeComponent();
-            // Proje içine atılmış repositoryde değişiklik yapıp pushluyorumuuum has ya ol
-
-            //buda benden sanaa
+            
         }
     }
 }
